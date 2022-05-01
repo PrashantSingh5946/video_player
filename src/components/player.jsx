a3d3650154e2a7b9fc18fde12a5286def71c13ed
Prashant Singh <v-singhpras@microsoft.com>
2022-05-01 16:44:54 +0530

    Adding the control bar buttons

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,42 +1,76 @@
 import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faForwardStep,
+  faVolumeHigh,
+  faPause,
+} from "@fortawesome/free-solid-svg-icons";
 import classes from "./styles.module.css";
 
 export default function Player(props) {
-    const videoRef = useRef();
-    const [isVideoPlaying,setVideoPlaying] = useState(false);
+  const videoRef = useRef();
+  const [isVideoPlaying, setVideoPlaying] = useState(false);
+  const playButtonRef = useRef();
+  const [passedDuration, setPassedDuration] = useState(0);
 
-    const playbackToggle = () =>
-    {
-        //play and pause functionality
-        setVideoPlaying((isVideoPlaying) => !isVideoPlaying);
+  //play pause functionality
+  const playbackToggle = () => {
+    //play and pause functionality
+    setVideoPlaying((isVideoPlaying) => !isVideoPlaying);
+  };
+
+  const updatePlaybackDuration = () => {
+    setPassedDuration(videoRef.current.currentTime);
+    console.log(passedDuration);
+  };
+  //handle play pause
+  useEffect(() => {
+    if (isVideoPlaying) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
     }
+  }, [isVideoPlaying]);
 
-    //handle play pause 
-    useEffect( ()=>{
-        
-        if(isVideoPlaying)
-        {
-            videoRef.current.play();
-        }
-        else
-        {
-            videoRef.current.pause();
-        }
+  //autoplay on startup
 
-    }, [isVideoPlaying]);
+  //update time on play
 
-
-    useEffect(()=>{setTimeout(()=>{videoRef.current.click(); setVideoPlaying(true)},500)},[]);
   return (
     <div className={classes.player}>
       <div className={classes["video-wrapper"]}>
-        <video src={props.url} ref={videoRef} />
+        <video src={props.url} ref={videoRef} autoPlay />
       </div>
-      <div className={classes["video-cover"]}>
+      <div className={classes["video-overlay"]}>
+        <div className={classes["video-cover"]}>
           <div className={classes["left"]}></div>
-          <div className={classes["play"]} onClick={playbackToggle}></div>
+          <div
+            className={classes["play"]}
+            onClick={playbackToggle}
+            onDurationChange={updatePlaybackDuration}
+            ref={playButtonRef}
+          ></div>
           <div className={classes["right"]}></div>
         </div>
+        <div className={classes["control-bar"]}>
+          <div className={classes["left-icons"]}>
+            <span onClick={playbackToggle}>
+              {isVideoPlaying ? (
+                <FontAwesomeIcon icon={faPause} />
+              ) : (
+                <FontAwesomeIcon icon={faPlay} />
+              )}
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faForwardStep} />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faVolumeHigh} />
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
