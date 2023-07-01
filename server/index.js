e886852bb13606e9fbf37cc9bef013435084e7bc
prashant singh <prashant.singh@softway.com>
2023-07-01 17:37:36 +0530

    Chat feature working

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const request = require('request')
 const WebSocket = require('ws');
 
@@ -8,6 +8,11 @@ const app = express();
 const clients = new Set();
 
 app.use(express.static('client'));
+
+const state = {
+    playbackToggle : false,
+    updatePlaybackDuration: 0,
+}
 
 
 app.get('/', (req, res) => {
@@ -26,10 +31,12 @@ wss.on('connection', (ws) => {
 
     clients.add(ws)
 
-    ws.on('message', (message) => {
-        console.log('Received message:', message);
-        sendToAllClients(message);
+    ws.send(JSON.stringify({type:"event", payload: state}))
+
+    ws.on('message', async (event) => {       
         // Process the message as needed
+        
+        handleMessage(event.toString());
     });
 
     ws.on('close', () => {
@@ -43,3 +50,59 @@ const sendToAllClients = (message) => {
         client.send(message);
     });
 };
+
+const handleMessage = (passedMessage) => {
+
+    let message = JSON.parse(passedMessage);
+
+    console.log(message)
+    
+    if(message.hasOwnProperty("chat"))
+    {
+        sendToAllClients(passedMessage);
+    }
+    else if (message.hasOwnProperty("event"))
+    {
+        let payload = {
+            ...message
+        }
+
+        if(payload.event == "updatePlaybackDuration")
+        {
+            state.updatePlaybackDuration < payload.data ? state.updatePlaybackDuration = payload.data : null;
+        }
+
+        if(payload.event == "seek")
+        {
+            state.updatePlaybackDuration = payload.data
+
+            sendToAllClients(JSON.stringify({type:"event", payload: {"updatePlaybackDuration": message.data}}))
+        }
+
+        else if(payload.event == "playbackToggle")
+        {
+            state.playbackToggle = payload.playbackToggle
+        }
+
+       //sendToAllClients({type:"event", payload: state})
+
+
+    }
+
+}
+
+
+//Schema
+
+// message = {
+
+//     type: ["event","connect","chat"],
+//     payload: {}
+// }
+
+
+// state = {
+//     playbackToggle: [true, false],
+//     updatePlaybackDuration: "",
+// }
+
